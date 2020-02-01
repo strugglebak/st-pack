70c3103fc5b5b0a5519483ca0f3624a4a239b84a
moss <dxp157@sohu.com>
2020-02-01 17:17:54 +0800

    完成 st-pack 的分析以及处理框架

--- bin/st-pack.js
+++ bin/st-pack.js
@@ -1,3 +1,11 @@
 #! /usr/bin/env node
 
-console.log('hello world')
+const path = require('path')
+const config = require(path.resolve(__dirname, 'webpack.config.js'))
+
+// 找到项目目录下的 webpack.config.js 然后通过一个 compiler 解析它
+const Complier = require('./lib/complier.js')
+const complier = new Complier(config)
+
+// 调用 compiler 的 run 方法执行代码
+complier.run()
