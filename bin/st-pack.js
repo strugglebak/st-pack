1fb9fd0109cdec9488ed49b9c9583c877b1eff2a
moss <dxp157@sohu.com>
2020-02-02 17:27:09 +0800

    修复不能找到文件的 bug

--- bin/st-pack.js
+++ bin/st-pack.js
@@ -1,10 +1,12 @@
 #! /usr/bin/env node
 
 const path = require('path')
-const config = require(path.resolve(__dirname, 'webpack.config.js'))
+const config = require(path.resolve('webpack.config.js'))
+
+console.log(config)
 
 // 找到项目目录下的 webpack.config.js 然后通过一个 compiler 解析它
-const Complier = require('./lib/complier.js')
+const Complier = require('../lib/compiler.js')
 const complier = new Complier(config)
 
 // 调用 compiler 的 run 方法执行代码
