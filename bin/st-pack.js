2cc3b570ed7a360d4b5e7caa378d70f5271c2533
moss <dxp157@sohu.com>
2020-02-03 10:49:16 +0800

    update

--- bin/st-pack.js
+++ bin/st-pack.js
@@ -3,8 +3,6 @@
 const path = require('path')
 const config = require(path.resolve('webpack.config.js'))
 
-console.log(config)
-
 // 找到项目目录下的 webpack.config.js 然后通过一个 compiler 解析它
 const Complier = require('../lib/compiler.js')
 const complier = new Complier(config)
